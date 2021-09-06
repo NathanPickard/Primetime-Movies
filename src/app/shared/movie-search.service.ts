dcf5222bb49a1a94d83d664746f78025e02715ad
Nathan Pickard <nathan.pickard87@gmail.com>
2021-09-06 11:48:26 -0700

    Movie search service comments

--- src/app/shared/movie-search.service.ts
+++ src/app/shared/movie-search.service.ts
@@ -19,26 +19,31 @@ export class MovieSearchService {
     );
   }
 
+  // Gets currently showing movies
   getMoviesNowPlaying() {
     return this.httpClient.get<any>(this.TMDB_API_URL + 'movie/now_playing?api_key=' + this.TMDB_API_KEY
       + '&language=en-US&page=1&region=US');
   }
 
+  // Gets upcoming movies
   getMoviesUpcoming() {
     return this.httpClient.get<any>(this.TMDB_API_URL + 'movie/upcoming?api_key=' + this.TMDB_API_KEY
       + '&language=en-US&page=1&region=US');
   }
 
+  // Gets most popular movies
   getMoviesMostPopular() {
     return this.httpClient.get<any>(this.TMDB_API_URL + 'discover/movie?sort_by=popularity.desc&api_key=' + this.TMDB_API_KEY
       + '&language=en-US&page=1&region=US');
   }
 
+  // Gets user's search results
   getMovieSearch(query) {
     return this.httpClient.get(this.TMDB_API_URL + 'search/movie?api_key=' + this.TMDB_API_KEY
       + '&language=en-US&page=1&region=US&query=' + query + '&page=1&include_adult=false');
   }
 
+  // Gets movie details based on movieId
   getMovieDetails(movieId) {
     return this.httpClient.get(this.TMDB_API_URL + 'movie/' + movieId + '?api_key=' + this.TMDB_API_KEY
       + '&language=en-US&page=1&region=US');
